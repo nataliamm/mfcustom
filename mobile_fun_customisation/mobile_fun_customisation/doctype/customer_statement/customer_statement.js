0e574c50d5948b70fd0575c680655ff86a9ded35
nataliamm <nataliamyshuk@gmail.com>
2018-02-27 23:34:32 +0200

    Changed the method.

--- mobile_fun_customisation/mobile_fun_customisation/doctype/customer_statement/customer_statement.js
+++ mobile_fun_customisation/mobile_fun_customisation/doctype/customer_statement/customer_statement.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Statement', {
-	refresh: function(frm) {
+	onload: function(frm) {
 		return frappe.call({
 			method: "get_customers",
 			doc: frm.doc,
